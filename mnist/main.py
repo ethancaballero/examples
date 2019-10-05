62f7900ec2d2596d24431af1a6164237e3551f47
Ethan Caballero <ethan.victor.caballero@gmail.com>
2019-10-05 00:41:07 -0400

    1st working MNIST myia pytorch example

--- mnist/main.py
+++ mnist/main.py
@@ -6,6 +6,63 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
+from myia import myia, value_and_grad
+from myia.ir import sexp_to_node
+from myia.lib import setter_from_getter
+from myia.abstract import Macro, build_value, macro
+from myia.frontends import activate_frontend
+
+from myia.debug import traceback
+
+activate_frontend('pytorch')
+
+from myia.operations import primitives as P
+from dataclasses import dataclass
+import numpy as np
+
+
+@dataclass
+class Optimizer:
+    parameter_names: tuple
+    lr: float
+
+    def __init__(self, parameter_names, lr):
+        self.parameter_names = tuple(tuple(_p[0].split('.'))
+                                              for _p in parameter_names)
+        self.lr = np.float32(lr)
+        self.momentum = 0.5
+
+    def update_rule(self, p, g):
+        return p - self.lr * g
+
+    def __call__(self, model, dmodel):
+        return self.update(self.parameter_names, model, dmodel, self.update_rule)
+
+    @macro
+    async def update(info, self_ref, param_names_ref, model_ref, dmodel_ref, update_rule_ref):
+        param_names_cst = build_value(await param_names_ref.get())
+        #self = build_value(await self_ref.get())
+        new_model = model_ref.node
+        dmodel = dmodel_ref.node
+        update_rule = update_rule_ref.node
+        model_abs = await model_ref.get()
+
+        for k in param_names_cst:
+            p = new_model
+            g = dmodel
+            for c in k:
+                p = (P.record_getitem, p, c)
+                g = (P.record_getitem, g, c)
+
+            p_node = sexp_to_node(p, info.graph)
+            g_node = sexp_to_node(g, info.graph)
+
+            pn = info.graph.apply(update_rule, p_node, g_node)
+
+            new_model = sexp_to_node(setter_from_getter(p, pn), info.graph)
+
+        return new_model
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -20,24 +77,51 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
+        #x = x.view(-1, 4*4*50)
+        x = x.view(-1, 800)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+backend = 'pytorch'
+backend_options = {'device': 'cpu'}
+
+def cost(model, data, target):
+    output = model(data)
+    return F.nll_loss(output, target)
+
+
+@myia(backend=backend, backend_options=backend_options, specialize_values=["model", "optimizer"])
+def step(model, data, target, optimizer):
+    loss, dmodel = value_and_grad(cost, 'model')(model, data, target)
+    return loss, optimizer(model, dmodel)
     
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
+        """
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+        #"""
+        loss, model = step(model, data, target,  optimizer)
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+    return model, optimizer
+
+
+@myia(backend=backend, backend_options=backend_options, specialize_values=["model"])
+def step_eval(model, data, target):
+    output = model(data)
+    test_loss = F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+    pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+    correct = pred.eq(target.view_as(pred)).sum().item()
+    return test_loss, correct
 
 def test(args, model, device, test_loader):
     model.eval()
@@ -46,10 +130,17 @@ def test(args, model, device, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
+            """
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            #test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            test_loss = test_loss + F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            #correct += pred.eq(target.view_as(pred)).sum().item()
+            correct = correct + pred.eq(target.view_as(pred)).sum().item()
+            """
+            test_loss_iter, correct_iter = step_eval(model, data, target)
+            test_loss = test_loss + test_loss_iter
+            correct = correct + correct_iter
 
     test_loss /= len(test_loader.dataset)
 
@@ -101,12 +192,12 @@ def main():
                        ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-
     model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    #optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = Optimizer(model.named_parameters(), lr=args.lr)
 
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+        model, optimizer = train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
 
     if (args.save_model):
